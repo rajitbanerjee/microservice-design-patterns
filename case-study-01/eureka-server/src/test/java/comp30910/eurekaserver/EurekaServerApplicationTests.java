09b2ffcb0fee4e6c1c48bff3781abf7c7013c1eb
Rajit Banerjee <rajit.banerjee@ucdconnect.ie>
2022-02-06 15:18:42 +0000

    Add eureka server

---
+++ case-study-01/eureka-server/src/test/java/comp30910/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,11 @@
+package comp30910.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = EurekaServerApplicationTests.class)
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
