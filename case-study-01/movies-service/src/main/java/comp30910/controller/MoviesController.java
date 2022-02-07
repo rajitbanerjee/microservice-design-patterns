f8582ce2a545c25637c61dec5859108e55552874
Rajit Banerjee <rajit.banerjee@ucdconnect.ie>
2022-02-07 09:42:39 +0000

    Implement basic api gateway

--- case-study-01/movies-service/src/main/java/comp30910/controller/MoviesController.java
+++ case-study-01/movies-service/src/main/java/comp30910/controller/MoviesController.java
@@ -7,18 +7,20 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/movies")
 public class MoviesController {
     private final MoviesService moviesService;
 
     @ResponseBody
-    @PostMapping(value = "/movie")
-    public MoviesDocument movie(@RequestBody MovieRequest request) {
+    @PostMapping("/find")
+    public MoviesDocument find(@RequestBody MovieRequest request) {
         MoviesDocument result = moviesService.findMovie(request.getName(), request.getCinemaName());
         if (result == null) throw new MovieNotFoundException();
         return result;
@@ -26,7 +28,7 @@ public class MoviesController {
 
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
     class MovieNotFoundException extends RuntimeException {
-        static final long serialVersionUID =    -6516152229878843037L;
+        static final long serialVersionUID = -6516152229878843037L;
     }
 }
 
--- case-study-01/post.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curl -X POST -H "Content-Type: application/json" \
-    -d '{"name": "Belfast", "cinemaName": "ODEON Stillorgan"}' \
-    http://localhost:8081/movie
